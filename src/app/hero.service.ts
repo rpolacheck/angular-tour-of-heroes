3f67c4468fb38ef6ef6ee9ca2f05db255e678cce
rpolacheck <rpolacheck@gmail.com>
2018-12-05 09:42:19 -0600

    Add HTTP and Delete Mock Heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
-
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { },
+    private log(message: string) {
+      this.messageService.add('HeroService: ${message}');
+    },
+    private heroesUrl = 'api/heroes'; // URL to web api
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of (HEROES);
+    return this.http.get<Hero[]>(this.hereosURL)
   }
 
   getHero(id: number): Observable<Hero> {
